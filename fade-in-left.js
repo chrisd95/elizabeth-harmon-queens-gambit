fa4ff6ae2dc8ce920daf499a471eabfde3a4dae3
chrisd95 <christopher.dip@umontreal.ca>
2020-12-02 13:03:37 -0500

    aight

---
+++ fade-in-left.js
@@ -0,0 +1,6 @@
+
+// I used both links to learn how to use fades:
+// https://stackoverflow.com/questions/43531747/jquery-fadein-with-opacity0-or-visibilityhidden
+// https://webdesign.tutsplus.com/tutorials/simple-fade-effect-on-scroll--cms-35166
+
+$("#formcontainer").fadeTo("slow",1);

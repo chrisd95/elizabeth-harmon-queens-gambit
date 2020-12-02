e388f6f0d3276a9d0887de3a379ba2601ad7ffc1
chrisd95 <christopher.dip@umontreal.ca>
2020-12-01 23:52:31 -0500

    look at that fade

--- fade.js
+++ fade.js
@@ -42,119 +42,101 @@ window.addEventListener("scroll", () => {
   currentScroll = window.pageYOffset
   if (currentScroll >= 350) {
     $("#carousel-fade").fadeTo("slow",1);
-  }
-  if(currentScroll >= 550){
-    $("#info-fade").fadeTo("slow",1);
+    setInterval(function(){
+      $("#info-fade").fadeTo("slow",1);
+    }, 500);
   }
   if(currentScroll >= 1100){
-    $("#section-title-2").fadeTo("slow",1);
-  }
-  if(currentScroll >= 1300){
-    $("#education-title-1").fadeTo("slow",1);
-    $("#education-description-1").fadeTo("slow",1);
-  }
-  if(currentScroll >= 1150){
-    $("#education-season-1").fadeTo("slow",1);
-    $("#education-year-1").fadeTo("slow",1);
-  }
-  if(currentScroll >= 1500){
-    $("#education-title-2").fadeTo("slow",1);
-    $("#education-description-2").fadeTo("slow",1);
-  }
-  if(currentScroll >= 1550){
-    $("#education-season-2").fadeTo("slow",1);
-    $("#education-year-2").fadeTo("slow",1);
-  }
+    setInterval(function(){
+      $("#section-title-2").fadeTo("slow",1);
+    }, 250);
+    setInterval(function(){
+      $("#education-title-1").fadeTo("slow",1);
+      $("#education-description-1").fadeTo("slow",1);
+      $("#education-season-1").fadeTo("slow",1);
+      $("#education-year-1").fadeTo("slow",1);
+    }, 750);
+    setInterval(function(){
+      $("#education-title-2").fadeTo("slow",1);
+      $("#education-description-2").fadeTo("slow",1);
+      $("#education-season-2").fadeTo("slow",1);
+      $("#education-year-2").fadeTo("slow",1);
+    }, 1250);
 
-  if(currentScroll >= 1650){
-    $("#education-title-3").fadeTo("slow",1);
-    $("#education-description-3").fadeTo("slow",1);
-  }
-  if(currentScroll >= 1700){
-    $("#education-season-3").fadeTo("slow",1);
-    $("#education-year-3").fadeTo("slow",1);
-  }
+    setInterval(function(){
+      $("#education-title-3").fadeTo("slow",1);
+      $("#education-description-3").fadeTo("slow",1);
+      $("#education-season-3").fadeTo("slow",1);
+      $("#education-year-3").fadeTo("slow",1);
+    }, 1750);
 
-  if(currentScroll >= 1800){
-    $("#education-title-4").fadeTo("slow",1);
-    $("#education-description-4").fadeTo("slow",1);
-  }
-  if(currentScroll >= 1850){
-    $("#education-season-4").fadeTo("slow",1);
-    $("#education-year-4").fadeTo("slow",1);
-  }
-  if(currentScroll >= 2000){
-    $("#section-title-3").fadeTo("slow",1);
+    setInterval(function(){
+      $("#education-title-4").fadeTo("slow",1);
+      $("#education-description-4").fadeTo("slow",1);
+      $("#education-season-4").fadeTo("slow",1);
+      $("#education-year-4").fadeTo("slow",1);
+    }, 2250);
+
   }
   if(currentScroll >= 2200){
-    $("#experience-title-1").fadeTo("slow",1);
-    $("#experience-description-1").fadeTo("slow",1);
+    setInterval(function(){
+      $("#section-title-3").fadeTo("slow",1);
+    }, 250);
+    setInterval(function(){
+      $("#experience-title-1").fadeTo("slow",1);
+      $("#experience-description-1").fadeTo("slow",1);
+      $("#experience-img-1").fadeTo("slow",1);
+    }, 750);
+    setInterval(function(){
+      $("#experience-img-2").fadeTo("slow",1);
+      $("#experience-title-2").fadeTo("slow",1);
+      $("#experience-description-2").fadeTo("slow",1);
+    }, 1250);
+    setInterval(function(){
+      $("#experience-title-3").fadeTo("slow",1);
+      $("#experience-description-3").fadeTo("slow",1);
+      $("#experience-img-3").fadeTo("slow",1);
+    }, 1750);
+    setInterval(function(){
+      $("#experience-img-4").fadeTo("slow",1);
+      $("#experience-title-4").fadeTo("slow",1);
+      $("#experience-description-4").fadeTo("slow",1);
+    }, 2250);
   }
-  if(currentScroll >= 2300){
-    $("#experience-img-1").fadeTo("slow",1);
-  }
-  if(currentScroll >= 2400){
-    $("#experience-img-2").fadeTo("slow",1);
-  }
-  if(currentScroll >= 2500){
-    $("#experience-title-2").fadeTo("slow",1);
-    $("#experience-description-2").fadeTo("slow",1);
-  }
-  if(currentScroll >= 2600){
-    $("#experience-title-3").fadeTo("slow",1);
-    $("#experience-description-3").fadeTo("slow",1);
-  }
-  if(currentScroll >= 2700){
-    $("#experience-img-3").fadeTo("slow",1);
-  }
+  
   if(currentScroll >= 2800){
-    $("#experience-img-4").fadeTo("slow",1);
-  }
-  if(currentScroll >= 2900){
-    $("#experience-title-4").fadeTo("slow",1);
-    $("#experience-description-4").fadeTo("slow",1);
-  }
-  if(currentScroll >= 3000){
-    $("#section-title-4").fadeTo("slow",1);
-  }
-  if(currentScroll >= 3200){
-    $("#ouverture-echecs-gif-1").fadeTo("slow",1);
-    $("#ouverture-echecs-1").fadeTo("slow",1);
-  }
-  if(currentScroll >= 3250){
-    $("#ouverture-echecs-gif-2").fadeTo("slow",1);
-    $("#ouverture-echecs-2").fadeTo("slow",1);
-
-  }
-  if(currentScroll >= 3300){
-    $("#ouverture-echecs-gif-3").fadeTo("slow",1);
-    $("#ouverture-echecs-3").fadeTo("slow",1);
-
-  }
-  if(currentScroll >= 3350){
-    $("#ouverture-echecs-gif-4").fadeTo("slow",1);
-    $("#ouverture-echecs-4").fadeTo("slow",1);
-
-  }
-  if(currentScroll >= 3425){
-    $("#ouverture-echecs-gif-5").fadeTo("slow",1);
-    $("#ouverture-echecs-5").fadeTo("slow",1);
-
-  }
-  if(currentScroll >= 3450){
-    $("#ouverture-echecs-gif-6").fadeTo("slow",1);
-    $("#ouverture-echecs-6").fadeTo("slow",1);
-
-  }
-  if(currentScroll >= 3475){
-    $("#ouverture-echecs-gif-7").fadeTo("slow",1);
-    $("#ouverture-echecs-7").fadeTo("slow",1);
-
-  }
-  if(currentScroll >= 3500){
-    $("#ouverture-echecs-gif-8").fadeTo("slow",1);
-    $("#ouverture-echecs-8").fadeTo("slow",1);
-
+    setInterval(function(){
+      $("#ouverture-echecs-gif-1").fadeTo("slow",1);
+      $("#ouverture-echecs-1").fadeTo("slow",1);
+    }, 500);
+    setInterval(function(){
+      $("#ouverture-echecs-gif-2").fadeTo("slow",1);
+      $("#ouverture-echecs-2").fadeTo("slow",1);
+    }, 750);
+    setInterval(function(){
+      $("#ouverture-echecs-gif-3").fadeTo("slow",1);
+      $("#ouverture-echecs-3").fadeTo("slow",1);
+    }, 1000);
+    setInterval(function(){
+      $("#ouverture-echecs-gif-4").fadeTo("slow",1);
+      $("#ouverture-echecs-4").fadeTo("slow",1);
+    }, 1250);
+    setInterval(function(){
+      $("#ouverture-echecs-gif-5").fadeTo("slow",1);
+      $("#ouverture-echecs-5").fadeTo("slow",1);
+    }, 1500);
+    setInterval(function(){
+      $("#ouverture-echecs-gif-6").fadeTo("slow",1);
+      $("#ouverture-echecs-6").fadeTo("slow",1);
+    }, 1750);
+    setInterval(function(){
+      $("#ouverture-echecs-gif-7").fadeTo("slow",1);
+      $("#ouverture-echecs-7").fadeTo("slow",1);
+    }, 2000);
+    setInterval(function(){
+      $("#ouverture-echecs-gif-8").fadeTo("slow",1);
+      $("#ouverture-echecs-8").fadeTo("slow",1);
+    }, 2250);
   }
 
 
